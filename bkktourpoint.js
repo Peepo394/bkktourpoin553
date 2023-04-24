8a1036a5cdfd89bf3214d238e381622c8747b83c
Peepo394 <124545861+Peepo394@users.noreply.github.com>
2023-04-24 11:00:16 +0700

    Add files via upload

---
+++ bkktourpoint.js
@@ -0,0 +1,155 @@
+var bkktourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระแก้ว",
+          "type": "Culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.49267126866891,
+            13.751487184343219
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ไอคอนสยาม",
+          "type": "Special",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.51026981298469,
+            13.726750278267332
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เยาราช",
+          "type": "Special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.50781491314683,
+            13.741561139101393
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนลุมพินี",
+          "type": "Natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.54147578744266,
+            13.73136461659341
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนป่าเบญจกิติ",
+          "type": "Natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.55579456731823,
+            13.729132202478226
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ซาฟารีเวิลด์",
+          "type": "Special",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.70314422571624,
+            13.865120011869536
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มิวเซียมสยาม",
+          "type": "Special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.49411081479036,
+            13.744105481116435
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดนัดจตุจักร",
+          "type": "Special",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.5504427152145,
+            13.800057555516887
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สยาม",
+          "type": "Special",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.53288205865846,
+            13.745909770777303
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดอรุณราชวรารามราชวรมหาวิหาร",
+          "type": "Culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.4884902150277,
+            13.743862227153713
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
